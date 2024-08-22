53738e4cdeb10d16b0612c41b43e333527948cda
Syldriem <93327634+Syldriem@users.noreply.github.com>
2024-08-22 15:13:36 +0200

    Add project files.

---
+++ MovieCardsAPI/Models/Entities/Actor.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieCardsAPI.Models.Entities
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
---
+++ MovieCardsAPI/Models/Entities/ContactInformation.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieCardsAPI.Models.Entities
+{
+    public class ContactInformation
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public int phoneNumber { get; set; }
+    }
+}
